0685f6b7123dc1ef9062dae5fb76fc2d48bb7744
binh.buidang <dangbinh4869@gmail.com>
2026-04-13 12:08:34 +0700

    update triplets.

--- cmake/triplets/arm64-osx.cmake
+++ cmake/triplets/arm64-osx.cmake
@@ -6,5 +6,4 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_OSX_DEPLOYMENT_TARGET "14.0")
 
-# Use Breakpad (in-process) instead of Crashpad (out-of-process) for sentry-native
 set(SENTRY_BACKEND "breakpad")
